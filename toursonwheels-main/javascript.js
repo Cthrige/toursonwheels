166ef44c74604eed90559d3feff53b8875a92587
Cthrige <90307121+Cthrige@users.noreply.github.com>
2022-04-01 09:49:27 +0200

    Add files via upload

---
+++ toursonwheels-main/javascript.js
@@ -0,0 +1,76 @@
+// Get the video
+var video = document.getElementById("myVideo");
+
+// Get the button
+var btn = document.getElementById("myBtn");
+
+// Pause and play the video, and change the button text
+function myFunction() {
+  if (video.paused) {
+    video.play();
+    btn.innerHTML = "Pause";
+  } else {
+    video.pause();
+    btn.innerHTML = "Play";
+  }
+}
+
+
+
+const body = document.body;
+const documentElement = document.documentElement;
+const progressBarEl = document.getElementById('progressBar');
+
+const scrollTop = () => Math.max(body.scrollTop, documentElement.scrollTop);
+const handleOnScroll = () => {
+  const scrollAmount = scrollTop() / (documentElement.scrollHeight - documentElement.clientHeight);
+  const scrollPercentage = Math.round(scrollAmount * 100);
+  
+  progressBarEl.style.width = `${scrollPercentage}%`
+};
+
+window.addEventListener('scroll', handleOnScroll);
+
+
+
+/* Scroll content - slider ind fra siden */
+const scrollElements = document.querySelectorAll(".js-scroll");
+
+const elementInView = (el, dividend = 1) => {
+  const elementTop = el.getBoundingClientRect().top;
+
+  return (
+    elementTop <=
+    (window.innerHeight || document.documentElement.clientHeight) / dividend
+  );
+};
+
+const elementOutofView = (el) => {
+  const elementTop = el.getBoundingClientRect().top;
+
+  return (
+    elementTop > (window.innerHeight || document.documentElement.clientHeight)
+  );
+};
+
+const displayScrollElement = (element) => {
+  element.classList.add("scrolled");
+};
+
+const hideScrollElement = (element) => {
+  element.classList.remove("scrolled");
+};
+
+const handleScrollAnimation = () => {
+  scrollElements.forEach((el) => {
+    if (elementInView(el, 1.25)) {
+      displayScrollElement(el);
+    } else if (elementOutofView(el)) {
+      hideScrollElement(el)
+    }
+  })
+}
+
+window.addEventListener("scroll", () => { 
+  handleScrollAnimation();
+});
