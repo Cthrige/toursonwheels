c6e1e1bae3951f6b5ea3f7953d45d50cbf576cb9
Cthrige <90307121+Cthrige@users.noreply.github.com>
2022-04-04 13:18:23 +0200

    Add files via upload

--- toursonwheels-main/javascript.js
+++ toursonwheels-main/javascript.js
@@ -1,6 +1,9 @@
 // Get the video
 var video = document.getElementById("myVideo");
 
+document.getElementById('vid').play();
+
+
 // Get the button
 var btn = document.getElementById("myBtn");
 
@@ -73,3 +76,28 @@ const handleScrollAnimation = () => {
 window.addEventListener("scroll", () => { 
   handleScrollAnimation();
 });
+
+
+/* Fact generator */
+var factPlaceholder = document.getElementById("cat-fact");
+var showFact = document.getElementById("show-fact");
+
+/* Facts from this API: https://fact.birb.pw/api/v1/cat */
+
+var CatFacts = ["I de 34 udviklede lande anslås det, at 1 % eller 10.000.000 mennesker har brug for en kørestol",
+               "I de 156 udviklingslande anslås det, at mindst 2 % eller 121.800.000 mennesker har brug for en kørestol",
+               "Samlet set, af de 7.091.500.000 mennesker i verden, kræver cirka 131.800.000 eller 1,85 % en kørestol",
+               "Det anslås, at yderligere 110 millioner mennesker verden over har brug for kørestole, men har dem ikke – 500.000 i udviklede lande og 109,5 millioner i udviklingslande",]
+
+var factNumber;
+
+function randomFact(){
+  return CatFacts[factNumber];
+}
+
+showFact.addEventListener('click', function(){
+  factNumber = Math.floor(Math.random()*5);
+  factPlaceholder.textContent = randomFact();
+});
+
+
